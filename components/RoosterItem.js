301575048535b835705e21e88a21fdeecae179ce
Your Name <you@example.com>
2023-06-22 13:33:09 +0200

    added padding for roosteritem

--- components/RoosterItem.js
+++ components/RoosterItem.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const RoosterItem = props => {
 
     return (
-        <View>
+        <View style={styles.Roosterdata}>
             <Text>{props.data.t}</Text>
             <Text>{props.data.v}</Text>
             <Text>{props.data.r}</Text>
@@ -13,3 +13,9 @@ const RoosterItem = props => {
 };
 
 export default RoosterItem;
+
+const styles = StyleSheet.create({
+    Roosterdata: {
+        padding: 5
+    }
+});
