f36b098a67f38b2a9d015d4d88da167ca6ead630
Your Name <you@example.com>
2023-06-23 08:53:57 +0200

    resized logo and more styling

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Rooster from './components/Rooster.js';
 
@@ -10,7 +9,6 @@ export default function App() {
         <ScrollView>
           <Rooster key="r-1"/>
         </ScrollView>
-        <StatusBar style="auto" />
       </View>
     );
   } catch (err) {
@@ -22,12 +20,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 40,
   },
   title: {
     fontWeight: 'bold',
     backgroundColor: 'orange',
     fontSize: 20,
     textAlign: 'center',
+    paddingTop: 40,
+    paddingBottom: 10
   }
 });
