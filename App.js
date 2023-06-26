4c8e64a66e7683a3f9a6d790f2e019d168e6a3ec
Your Name <you@example.com>
2023-06-26 14:53:21 +0200

    fixed more styling plus more data on screen

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, Button, SafeAreaView } from 'react-native';
 import Rooster from './components/Rooster.js';
 
 export default function App() {
   try {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Deltion Rooster SD2A</Text>
         <ScrollView>
           <Rooster key="r-1"/>
         </ScrollView>
-      </View>
+        <Button title="info" color="orange" onPress={() => {alert(`Dit is de onofficiële rooster app voor Deltion \n\nVoor vragen stuur me een dm op Discord (dhulsman889)`)}}/>
+      </SafeAreaView>
     );
   } catch (err) {
     console.log(err);
@@ -19,14 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
   },
   title: {
     fontWeight: 'bold',
     backgroundColor: 'orange',
     fontSize: 20,
     textAlign: 'center',
-    paddingTop: 10,
-    paddingBottom: 10
-  }
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
 });
--- components/Rooster.js
+++ components/Rooster.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Button } from "react-native";
 import RoosterItem from "./RoosterItem";
 import axios from "axios";
 
-
 const Rooster = () => {
     try {
+
         const [schedule, setSchedule] = useState([]);
         var currentDate = new Date();
+
+        //startdate
         var diff = currentDate.getDay() - 1;
         if (diff < 0) {
             diff = 6;
         }
+        
         var startDate = new Date(currentDate.setDate(currentDate.getDate() - diff));
         var formattedStartDate = startDate.getFullYear() + ('0' + (startDate.getMonth() + 1)).slice(-2) + ('0' + startDate.getDate()).slice(-2);
-        var diff = 8 - currentDate.getDay();
-        if (diff < 0) {
-            diff += 7;
+
+        //enddate
+        let diff2 = 15 - currentDate.getDay();
+        if (diff2 < 0) {
+            diff2 += 7;
         }
-        var endDate = new Date(currentDate.setDate(currentDate.getDate() + diff));
+
+        var endDate = new Date(currentDate.setDate(currentDate.getDate() + diff2));
         var formattedEndDate = endDate.getFullYear() + ('0' + (endDate.getMonth() + 1)).slice(-2) + ('0' + endDate.getDate()).slice(-2);
 
         const Data = async () => {
